b0c653a5e99f1d908570bde8a24d13e82e2e4f8e
name here <email here>
2014-04-03 17:09:38 +0530

    mansi,k1| start building tw fork of dhis2

--- update_code.sh
+++ update_code.sh
@@ -1,7 +1,8 @@
-DIRECTORY="dhis2"
+DIRECTORY="tw-trunk"
 if [ -d "$DIRECTORY" ]; then
 	cd $DIRECTORY;
 	bzr update;
 else
-	 bzr branch lp:~dhis2-devs-core/dhis2/2.14 $DIRECTORY;
+	 #bzr branch lp:~dhis2-devs-core/dhis2/2.14 $DIRECTORY;
+	 bzr branch lp:~kiran-f/dhis2/dhis2 $DIRECTORY;
 fi
