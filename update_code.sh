3fa60fa1449d386872ac9093b2251b72c4889521
Kiran Prakash <kiran.in@gmail.com>
2014-09-18 16:06:01 +0530

    k1/simij | | building dhis trunk.

--- update_code.sh
+++ update_code.sh
@@ -1,9 +1,9 @@
-DIRECTORY="tw-trunk"
+DIRECTORY="trunk"
 if [ -d "$DIRECTORY" ]; then
 	cd $DIRECTORY;
 	bzr update;
 else
-	 #bzr branch lp:~dhis2-devs-core/dhis2/2.14 $DIRECTORY;
-	 bzr branch lp:~tw-msf/dhis2/dhis2 $DIRECTORY;
+	 bzr branch lp:~dhis2-devs-core/dhis2/trunk $DIRECTORY;
+	 # bzr branch lp:~tw-msf/dhis2/dhis2 $DIRECTORY;
      cd $DIRECTORY;
 fi
