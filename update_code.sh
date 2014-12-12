51aa6b6367f0eeed49f1d5957ea46d4396f37890
Simanta J Handique <shandique@thoughtworks.com>
2014-12-12 11:09:59 +0530

    Mansi, Simanta | Pointing DHIS to 2.17

--- update_code.sh
+++ update_code.sh
@@ -1,9 +1,8 @@
-DIRECTORY="trunk"
+DIRECTORY="dhis217"
 if [ -d "$DIRECTORY" ]; then
 	cd $DIRECTORY;
 	bzr update;
 else
-	 bzr branch lp:~dhis2-devs-core/dhis2/trunk $DIRECTORY;
-	 # bzr branch lp:~tw-msf/dhis2/dhis2 $DIRECTORY;
+	 bzr branch lp:~dhis2-devs-core/dhis2/2.17 $DIRECTORY;
      cd $DIRECTORY;
 fi
