879bfeed19440811e7132680ddbb6da7e46edad3
Simanta J Handique <shandique@thoughtworks.com>
2015-04-02 15:06:42 +0530

    Simanta | #000 | Moving back to tw-trunk

--- update_code.sh
+++ update_code.sh
@@ -3,8 +3,8 @@ if [ -d "$DIRECTORY" ]; then
 	cd $DIRECTORY;
 	bzr update;
 else
-	 bzr branch lp:~dhis2-devs-core/dhis2/trunk $DIRECTORY;
-	 #bzr branch lp:~tw-msf/dhis2/dhis2 $DIRECTORY;
+	 #bzr branch lp:~dhis2-devs-core/dhis2/trunk $DIRECTORY;
+	 bzr branch lp:~tw-msf/dhis2/dhis2 $DIRECTORY;
 
      cd $DIRECTORY;
 fi
