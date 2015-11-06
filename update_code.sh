c610b13c90ed0c6247653d5457c7d3b4b8b89cdd
shital <shitalma@thoughtworks.com>
2015-11-06 11:49:09 +0530

    Revert "Simanta | #000 | Switching to DHIS 2.20"
    This reverts commit 2eaac30e8f87ef13891f43462e2f096d33483438.

--- update_code.sh
+++ update_code.sh
@@ -4,7 +4,7 @@ if [ -d "$DIRECTORY" ]; then
 	bzr update;
 else
 	 #bzr branch lp:~dhis2-devs-core/dhis2/trunk $DIRECTORY;
-         bzr branch lp:~dhis2-devs-core/dhis2/2.20 $DIRECTORY;
+         bzr branch lp:~dhis2-devs-core/dhis2/2.19 $DIRECTORY;
 	 #bzr branch lp:~tw-msf/dhis2/dhis2 $DIRECTORY;
 
      cd $DIRECTORY;
