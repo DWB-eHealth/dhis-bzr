b4fb894384b7a826ffe6f236184f5b4927156c27
harikrishnakanchi <harikrishnakanchi@gmail.com>
2016-01-12 17:23:49 +0530

    Hari, David | #1591 | Updated the dhis revision number for 2.19 to fix week time period issue.

--- update_code.sh
+++ update_code.sh
@@ -3,9 +3,6 @@ if [ -d "$DIRECTORY" ]; then
 	cd $DIRECTORY;
 	bzr update;
 else
-	 #bzr branch lp:~dhis2-devs-core/dhis2/trunk $DIRECTORY;
-	 bzr branch lp:~dhis2-devs-core/dhis2/2.21 $DIRECTORY;
-         #bzr branch lp:~dhis2-devs-core/dhis2/2.19 -r 19265 $DIRECTORY;
-
-     cd $DIRECTORY;
+    bzr branch lp:~dhis2-devs-core/dhis2/2.19 -r 19287 $DIRECTORY;
+    cd $DIRECTORY;
 fi
