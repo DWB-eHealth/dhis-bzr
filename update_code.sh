3bd9bd4785d6f4ee3209e87b4ca50615de3c7fe1
jhansireddy <jhansireddy007@gmail.com>
2016-03-29 11:47:57 +0530

    David, Jhansi | #1766 | Update dhis-bzr to 2.22 version

--- update_code.sh
+++ update_code.sh
@@ -1,11 +1,8 @@
 DIRECTORY="trunk"
 if [ -d "$DIRECTORY" ]; then
-	cd $DIRECTORY;
-	bzr update;
+  cd $DIRECTORY;
+  bzr update;
 else
-	 #bzr branch lp:~dhis2-devs-core/dhis2/trunk $DIRECTORY;
-	 bzr branch lp:~dhis2-devs-core/dhis2/2.21 $DIRECTORY;
-         #bzr branch lp:~dhis2-devs-core/dhis2/2.19 -r 19265 $DIRECTORY;
-
-     cd $DIRECTORY;
+  bzr branch lp:~dhis2-devs-core/dhis2/2.22 -r 21952 $DIRECTORY; 
+  cd $DIRECTORY;
 fi
