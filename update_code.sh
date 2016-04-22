ad21fcdbc1432bbe6d0b20db81e9b4dab3336415
Raghavendra <raghavr@thoughtworks.com>
2016-04-22 19:18:29 +0530

    Raghavendra, Jhansi | #1840 | Add temporary patch to fix the completeDataSetRegistrations api

--- update_code.sh
+++ update_code.sh
@@ -3,6 +3,6 @@ if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   bzr update;
 else
-  bzr branch lp:~dhis2-devs-core/dhis2/2.22 -r 21952 $DIRECTORY; 
+  bzr branch lp:~tw-msf/dhis2/v222_api_patch $DIRECTORY;
   cd $DIRECTORY;
 fi
