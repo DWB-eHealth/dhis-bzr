ce5ef9d0e156967fba9f5deb7aef35301189eca7
David Oh <doh@thoughtworks.com>
2016-06-15 12:06:25 +0530

    David | #0000 | Explicitly specifying revision number on bzr branch

--- update_code.sh
+++ update_code.sh
@@ -3,6 +3,6 @@ if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   bzr update;
 else
-  bzr branch lp:~tw-msf/dhis2/v222_api_patch $DIRECTORY;
+  bzr branch lp:~tw-msf/dhis2/v222_api_patch -r 21956 $DIRECTORY;
   cd $DIRECTORY;
 fi
