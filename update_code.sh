0e8725672764f2c3f30fa2b97f3fac3c14c935b0
David Oh <doh@thoughtworks.com>
2016-06-15 12:49:47 +0530

    David | #0000 | Updating revision number to include backport of Lars' fix for analytics task performance

--- update_code.sh
+++ update_code.sh
@@ -3,6 +3,6 @@ if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   bzr update;
 else
-  bzr branch lp:~tw-msf/dhis2/v222_api_patch -r 21956 $DIRECTORY;
+  bzr branch lp:~tw-msf/dhis2/v222_api_patch -r 21957 $DIRECTORY;
   cd $DIRECTORY;
 fi
