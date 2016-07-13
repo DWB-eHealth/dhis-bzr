7e71c174a666f10b5902a9a82c84d4ed6b1a8187
Prachi <prachisr@thoughtworks.com>
2016-07-13 16:31:53 +0530

    David, Prachi | #1975 | Updated dhis-bzr to DHIS2 v2.23

--- update_code.sh
+++ update_code.sh
@@ -3,6 +3,6 @@ if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   bzr update;
 else
-  bzr branch lp:~tw-msf/dhis2/v222_api_patch -r 21957 $DIRECTORY;
+  bzr branch lp:~dhis2-devs-core/dhis2/2.23 -r 23044 $DIRECTORY;
   cd $DIRECTORY;
 fi
