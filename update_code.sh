e70ab8ecdb7e3525b141599800918c9233b9aacb
David Oh <doh@thoughtworks.com>
2016-10-07 19:35:33 +0530

    Hari, David | #1801 | Updated script to point to TW fork of 2.23 branch

--- update_code.sh
+++ update_code.sh
@@ -1,11 +1,11 @@
 DIRECTORY="trunk"
-COMMIT_SHA="979a8e9952be410438563178f47d813f3bc0a60d"
+COMMIT_SHA="87df06349a8a51f9532bc484ceeeeb8291e35f1b"
 
 if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   git fetch && git checkout $COMMIT_SHA;
 else
-  git clone git@github.com:dhis2/dhis2-core.git $DIRECTORY;
+  git clone git@github.com:DWB-eHealth/dhis2-core.git $DIRECTORY;
   cd $DIRECTORY;
   git checkout $COMMIT_SHA;
 fi
