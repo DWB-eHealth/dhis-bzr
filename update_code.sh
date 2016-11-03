9e7ce03b53cca4f79e7d3a5d676434843ee0b212
jhansireddy <jhansireddy007@gmail.com>
2016-11-03 15:15:17 +0530

    Jhansi | #2193 | Upgraded revision number to 21958 which includes the data approval api fix

--- update_code.sh
+++ update_code.sh
@@ -3,6 +3,6 @@ if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   bzr update;
 else
-  bzr branch lp:~tw-msf/dhis2/v222_api_patch -r 21957 $DIRECTORY;
+  bzr branch lp:~tw-msf/dhis2/v222_api_patch -r 21958 $DIRECTORY;
   cd $DIRECTORY;
 fi
