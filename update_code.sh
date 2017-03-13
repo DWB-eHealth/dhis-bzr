5ee8f819d2f5b318549a1c87c7ceffd0c77e4469
Raghavendra <raghavr@thoughtworks.com>
2017-03-13 13:16:47 +0530

    Jhansi, Raghavendra | #2398 | Using forked DHIS repo

--- update_code.sh
+++ update_code.sh
@@ -5,7 +5,7 @@ if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   git fetch && git checkout $COMMIT_SHA;
 else
-  git clone git@github.com:dhis2/dhis2-core.git $DIRECTORY;
+  git clone git@github.com:DWB-eHealth/dhis2-core.git $DIRECTORY;
   cd $DIRECTORY;
   git checkout $COMMIT_SHA;
 fi
