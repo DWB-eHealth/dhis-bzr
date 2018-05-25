d393bafafaadf5402bec066f47cbb43e303d6721
Gaurav <gaurav.shahdeo@gmail.com>
2018-05-25 12:08:00 +0530

    Gaurav | #2740 | Updated the script to point to DHIS2 2.29 branch

--- update_code.sh
+++ update_code.sh
@@ -1,11 +1,11 @@
 DIRECTORY="trunk"
-COMMIT_SHA="f9fd2c9be3d337c3eae7af9d20a1965d4a8f1330"
+COMMIT_SHA=2.29
 
 if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   git fetch && git checkout $COMMIT_SHA;
 else
-  git clone git@github.com:DWB-eHealth/dhis2-core.git $DIRECTORY;
+  git clone git@github.com:dhis2/dhis2-core.git $DIRECTORY;
   cd $DIRECTORY;
   git checkout $COMMIT_SHA;
 fi
