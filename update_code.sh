fd265e33aa660d3d776d9e370c54beee11ff541a
Gaurav <gaurav.shahdeo@gmail.com>
2019-02-26 15:06:40 +0530

    Gaurav | #0000 | Updated commit SHA to point to latest DHIS 2.31

--- update_code.sh
+++ update_code.sh
@@ -1,11 +1,11 @@
 DIRECTORY="trunk"
-COMMIT_SHA="d9a3f9ab77878c158ebfc4cc33027bb125c950b1"
+COMMIT_SHA="0376b9588c478478b86773c6c5a77d4af4bf2b9f"
 
 if [ -d "$DIRECTORY" ]; then
   cd $DIRECTORY;
   git fetch && git checkout $COMMIT_SHA;
 else
-  git clone git@github.com:DWB-eHealth/dhis2-core.git $DIRECTORY;
+  git clone git@github.com:dhis2/dhis2-core.git $DIRECTORY;
   cd $DIRECTORY;
   git checkout $COMMIT_SHA;
 fi
